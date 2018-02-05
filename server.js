702d0232fdf67ef467b49f0960970a21ea6ab939
e9ine <e9ine@e9ines-MacBook-Pro-2.local>
2018-02-05 17:28:16 +0530

    changed to dynamic port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //packages import
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const bodyParser = require('body-parser');
 
 //config for mongoose added
